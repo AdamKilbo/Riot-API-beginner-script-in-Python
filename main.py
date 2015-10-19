895150867faa658ff4e669f1ac5209a80f4d73e9
Adam Kilbo <amkilbo@gmail.com>
2015-10-19 16:08:27 -0500

    Additional comments on the code

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from RiotAPI import RiotAPI
 
 def main():
 	api = RiotAPI('') # Your API key goes inbetween the blank quotes.
-	r = api.get_summoner_by_name('golden foxes')
-	print r
-	print r['goldenfoxes']
-	print r['goldenfoxes']['id']
+	r = api.get_summoner_by_name('golden foxes') # make a request to the API for information on the summoner name 'Golden Foxes' (my personal LoL summoner name). The variable R holds the JSON dictionary returned by the API.
+	print r # printing the whole JSON dictionary
+	print r['goldenfoxes'] # printing information on the summoner name Golden Foxes found within the JSON dictionary
+	print r['goldenfoxes']['id'] # printing the ID of Golden Foxes within the JSON dictionary
 
 if __name__ == "__main__":
 	main()
